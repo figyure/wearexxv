044dadae3c55a3b9fc2d522fdf3510a8e2205596
figyure <figge911@yahoo.com>
2015-03-25 10:14:42 -0600

    Upload 'Vinto_work_copy'
    Vinto Work Copy contains all files for the site. Need to see how they
    all react together live to move forward.

---
+++ Vinto_work_copy/documentation/assets/js/custom.js
@@ -0,0 +1,11 @@
+/**
+ *	jQuery scripts for ThemeShifters documentation
+ */
+jQuery(document).ready(function($) {
+	$('#main-nav').onePageNav({
+		currentClass: 'current-menu-item',
+		scrollSpeed: 800,
+		scrollOffset: 40,
+		scrollThreshold: 0.5
+	});
+});
